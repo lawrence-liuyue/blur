13aa02f6a20d59d6fcd073ab063d11c498b3c031
lawrence-liuyue <lawrence_aaa@126.com>
2021-11-22 10:55:32 +0800

    add file

---
+++ assets/scripts/RTSetter-xblur.ts
@@ -0,0 +1,31 @@
+
+import { _decorator, Component, Material, RenderTexture } from 'cc';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = RTSetter
+ * DateTime = Sat Nov 20 2021 15:31:42 GMT+0800 (中国标准时间)
+ * Author = lpfysp
+ * FileBasename = RTSetter.ts
+ * FileBasenameNoExtension = RTSetter
+ * URL = db://assets/scripts/RTSetter.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.3/manual/en/
+ *
+ */
+
+@ccclass('RTSetterXblur')
+export class RTSetterXblur extends Component {
+    @property(Material)
+    material: Material = null!;
+
+    @property(RenderTexture)
+    srcRT: RenderTexture = null!;
+
+    @property
+    texSlotName = 'mainTexture';
+
+    start () {
+        this.material.setProperty(this.texSlotName, this.srcRT);
+    }
+}
